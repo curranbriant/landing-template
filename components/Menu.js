aae0bdab5c47f5b9c9620a677126ad14fb03315e
Brian Curran <curranbriant@gmail.com>
2022-05-11 14:56:09 -0400

    readds mc

--- components/Menu.js
+++ components/Menu.js
@@ -26,7 +26,7 @@ export default function DropDown() {
                     leaveTo="transform opacity-0 scale-95"
                 >
                     <Menu.Items className="focus:outline-none absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5">
-                        <div className="px-1 py-1 flex flex-col">
+                        <div className="px-3 py-3 flex flex-col">
                             <Menu.Item>
                                 {({ active }) => (
                                     <Link href="https://twitter.com/ennntropy"
